b643172495e9dd1212c583de225997235a2aaf39
Jon Maneiro <jon.maneiro@ikasle.egibide.org>
2018-11-08 13:33:19 +0100

    Grid-Hecho PC

--- Web/index.php
+++ Web/index.php
@@ -3,18 +3,24 @@
     <head>
         <title>Aergibide S.L</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="css/grid-general.css">
+        <link href="css/grid-general.css" type="text/css" rel="stylesheet">
     </head>
     <body>
-      <!--  <header id="menu-cabecera">
+        <nav id="menu-cabecera">
             <img alt="Logo-Empresa">
             <a href="#">Pregunta</a>
             <form id="formulario-busqueda" method="get" action="#">
                 <label for="caja-busqueda" id="label-caja-busqueda">Buscar</label>
                 <input type="text" name="busqueda" id="caja-busqueda" placeholder="Buscar">
             </form>
-            <a><button>Iniciar Sesi&oacuten</button></a>
-        </header> -->
+            <a href="#"><button>Iniciar Sesi&oacuten</button></a>
+            <form id="formulario-usuario" method="get" action="#">
+                <select name="usuario-opciones" id="opciones-usuario-select">
+                    <option value="#">Perfil</option>
+                    <option value="#">Configuracion</option>
+                </select>
+            </form>
+        </nav>
         <div id="contenedor-principal">
 
 
