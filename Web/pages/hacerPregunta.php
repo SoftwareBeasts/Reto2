e03553ef6bf3de43bdd07be1d999beb3b251a5a2
Imanol <imanol.luis@ikasle.egibide.org>
2018-11-09 14:08:17 +0100

    perfil.php y hacerPregunta.php

--- Web/pages/hacerPregunta.php
+++ Web/pages/hacerPregunta.php
@@ -1,44 +1,49 @@
+<?php
+require "../php/generar-nav-footer.php";
+?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>Hacer una pregunta &#124; Aergibide S.L.</title>
+    <title>Haz tu pregunta &#124; Aergibide S.L.</title>
+    <link rel="stylesheet" type="text/css" href="../css/grid-general.css" />
     <link rel="stylesheet" type="text/css" href="../css/hacerPregunta.css" />
 </head>
 <body>
-<!--
-    <header id="menu-cabecera">
-        <img alt="Logo-Empresa">
-        <a href="#">Pregunta</a>
-        <form id="formulario-busqueda" method="get" action="#">
-            <label for="caja-busqueda" id="label-caja-busqueda">Buscar</label>
-            <input type="text" name="busqueda" id="caja-busqueda" placeholder="Buscar">
+
+<main id="contenedor-principal">
+    <?php
+    generarNav();
+    ?>
+    <section id="contenedor-hacer-preguntas">
+        <h1>Haz tu pregunta</h1>
+        <form action="" method="post">
+            <ul>
+                <li>
+                    <label for="tituloPregunta">T&iacute;tulo:</label>
+                    <input type="text" id="tituloPregunta" name="tituloPregunta" value="" />
+                </li>
+                <li>
+                    <textarea id="descripcionPregunta" name="descripcionPregunta" rows="15"></textarea>
+                </li>
+                <li>
+                    <label for="categoriasPregunta">Categor&iacute;as:</label>
+                    <input type="text" id="categoriasPregunta" name="categoriasPregunta" value="" />
+                </li>
+                <li>
+                    <input type="submit" id="enviarPregunta" value="Enviar" />
+                </li>
+            </ul>
         </form>
-        <a><button>Iniciar Sesi&oacuten</button></a>
-    </header>
--->
-<div id="contenedor">
-    <h1>Hacer una pregunta</h1>
-    <form action="" method="post">
-        <ul>
-            <li>
-                <label for="tituloPregunta">T&iacute;tulo:</label>
-                <input type="text" id="tituloPregunta" name="tituloPregunta" value="" />
-            </li>
-            <li>
-                <label for="descripcionPregunta">Descripci&oacute;n:</label>
-                <textarea id="descripcionPregunta" name="descripcionPregunta" rows="15" cols="90"></textarea>
-            </li>
-            <li>
-                <label for="etiquetasPregunta">Etiquetas:</label>
-                <input type="text" id="etiquetasPregunta" name="etiquetasPregunta" value="" />
-            </li>
-            <li>
-                <input type="submit" id="enviarPregunta" value="Enviar" />
-            </li>
-        </ul>
-    </form>
-</div>
+    </section>
+    <aside id="barra-lateral-index">
+
+    </aside>
+    <?php
+    generarFooter();
+    ?>
+</main>
+
 </body>
 </html>
