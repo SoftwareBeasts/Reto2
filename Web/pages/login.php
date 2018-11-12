8d36facb838ddb0cbd7a92f86a67208613e599ab
unaipuelles <unai-p@hotmail.com>
2018-11-12 12:40:23 +0100

    Añadido DB y modificación login.php

--- Web/pages/login.php
+++ Web/pages/login.php
@@ -1,5 +1,18 @@
 <?php
     require '../php/generar-nav-footer.php';
+    require '../php/db/usuarioDB.php';
+
+    session_start();
+
+    if(!isset($_SESSION['userLogged'])){
+        $_SESSION['userLogged'] = null;
+    }
+    $datosIncorrectos = false;
+    if(isset($_SESSION['contador'])) {
+        $_SESSION['contador']++;
+    } else {
+        $_SESSION['contador'] = 0;
+    }
 ?>
 <!DOCTYPE html>
 <html>
@@ -8,42 +21,70 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" href="../css/grid-general.css">
     <link rel="stylesheet" type="text/css" href="../css/login.css">
-    <?php
-        $encript = password_hash("unaipuelles", PASSWORD_DEFAULT);
-        echo $encript;
-        if(isset($_POST['pass'])){
-
-            if(password_verify($_POST['pass'], $encript)){
-                ?>
-                <p>CORRECTO</p>
-                <?php
-            }
-            else{
-                ?>
-                <p>INCORRECTO</p>
-                <?php
-            }
-        }
-    ?>
 </head>
 <body>
     <main id="contenedor-principal" onload="">
-        <?php generarNav(); ?>
+        <?php generarNav("../"); ?>
+    <?php
+    if(isset($_POST['usem']) && isset($_POST['pass'])){
+        $usuario = findUsuario($_POST['usem']);
+        if($usuario != null){
+            if(password_verify($_POST['pass'], $usuario['pass']))
+                $_SESSION['userLogged'] = $usuario;
+            else
+                $datosIncorrectos = true;
+        }
+        else{
+            $datosIncorrectos = true;
+        }
+    }
+
+    if(isset($_SESSION['userLogged']) || $_SESSION['contador'] == 0){
+        ?>
         <div class="center">
             <form method="post" action="login.php">
-                <div>
-                    <label class="top" for="usem">Usuario o Email</label>
-                    <input class="top" type="text" id="usem" placeholder="Usuario o Email">
-                </div>
-                <div>
-                    <label class="align center" for="pass">Contraseña</label>
-                    <input class="align center" type="password" id="pass" name="pass" placeholder="···············">
-                    <a href="#">Aún no estoy registrado</a>
-                </div>
-                <input class="bottom" type="submit">
+        <?php
+        if($_SESSION['userLogged'] == null){
+            ?>
+                    <div>
+                        <label class="top" for="usem">Usuario o Email</label>
+                        <input class="top" type="text" name="usem" id="usem" placeholder="Usuario o Email">
+                    </div>
+                    <div>
+                        <label class="align center" for="pass">Contraseña</label>
+                        <input class="align center" type="password" id="pass" name="pass" placeholder="···············">
+                        <a href="#">Aún no estoy registrado</a>
+                    </div>
+                    <?php
+                    if($_SESSION['contador'] > 2){
+                        ?>
+                        <input type="submit" value="Login" disabled="disabled">
+                        <?php
+                    }
+                    elseif ($datosIncorrectos){
+                        echo "Incorrecto";
+                        ?>
+                        <?php
+                    }
+                    else{
+                        ?>
+                        <input type="submit" value="Login">
+                        <?php
+                    }
+                    ?>
+            <?php
+        }
+        else{
+            header("location:/index.php");
+        }
+        ?>
             </form>
         </div>
+        <?php
+    }
+    ?>
+
         <?php generarFooter(); ?>
     </main>
-</body>
+    </body>
 </html>
