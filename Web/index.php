d3022635bd2be2d5a21083e588cbc954e6396ecd
Jon Maneiro <jon.maneiro@ikasle.egibide.org>
2018-11-12 10:24:27 +0100

    Cosas antes del recreo

--- Web/index.php
+++ Web/index.php
@@ -1,45 +1,61 @@
 <?php
-    require "php/generar-nav-footer.php";
+require "php/generar-nav-footer.php";
 ?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Aergibide S.L</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="css/grid-general.css" type="text/css" rel="stylesheet">
-        <link href="css/index.css" type="text/css" rel="stylesheet">
-    </head>
-    <body>
+<head>
+    <title>Aergibide S.L</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="css/grid-general.css" type="text/css" rel="stylesheet">
+    <link href="css/index.css" type="text/css" rel="stylesheet">
+</head>
+<body>
 
-        <main id="contenedor-principal">
-            <?php
-                generarNav();
-            ?>
+<main id="contenedor-principal">
+    <?php
+    generarNav();
+    ?>
 
-            <section id="contenedor-selectores-preguntas-index">
-                <div id="contenedor-selectores-index">
-                    <button class="boton-selector">Recientes</button>
-                    <button class="boton-selector">M&aacutes votadas</button>
-                    <button class="boton-selector">Sin Responder</button>
-                    <button class="boton-selector">Respondidas</button>
+    <section id="contenedor-selectores-preguntas-index">
+        <div id="contenedor-selectores-index">
+            <button class="boton-selector">Recientes</button>
+            <button class="boton-selector">M&aacute;s votadas</button>
+            <button class="boton-selector">Sin Responder</button>
+            <button class="boton-selector">Respondidas</button>
+        </div>
+        <div id="contenedor-preguntas-index">
+            <article class="pregunta-index" >
+                <span class="informacion-usuario-fecha-pregunta">por <a href="#" class="link-perfil-usuario">Unai Puelles</a> a 11 noviembre 2018</span>
+                <h2 class="titulo-pregunta">Como usar PHP</h2>
+                <div class="contenedor-categorias-pregunta">
+                    <label><a href="#">PHP</a></label>
                 </div>
-                <div id="contenedor-preguntas-index">
-                    <article class="pregunta-index">
-                        <span class="informacion-usuario-fecha-pregunta"><a href="#" class="link-perfil-usuario">Unai Puelles</a></span>
-                        <h2 class="titulo-pregunta">Como usar PHP</h2>
-                        <div class="contenedor-likes-preguntas">
-                            <button class="boton-like-pregunta"><img class="boton-like-imagen"></button>
-                            <
-                        </div>
-                    </article>
+                <div class="contenedor-likes-preguntas">
+                    <span class="puntuacion-pregunta-index">11</span>
                 </div>
-            </section>
-            <aside id="barra-lateral-index">
-                <div></div>
-            </aside>
-            <?php
-                generarFooter();
-            ?>
-        </main>
-    </body>
+            </article>
+        </div>
+    </section>
+    <aside id="barra-lateral-index">
+        <div id="placeholder-aside-index"></div>
+        <div id="contenedor-principal-categorias-index">
+            <h3 id="titulo-categoria">Categor&iacute;as</h3>
+            <div id="contenedor-secundario-categorias-index">
+                <label><a href="#">PHP</a></label>
+                <label><a href="#">PHP</a></label>
+                <label><a href="#">PHP</a></label>
+                <label><a href="#">PHP</a></label>
+                <label><a href="#">PHP</a></label>
+                <label><a href="#">PHP</a></label>
+                <label><a href="#">PHP</a></label>
+                <label><a href="#">PHP</a></label>
+                <label><a href="#">PHP</a></label>
+            </div>
+        </div>
+    </aside>
+    <?php
+    generarFooter();
+    ?>
+</main>
+</body>
 </html>
