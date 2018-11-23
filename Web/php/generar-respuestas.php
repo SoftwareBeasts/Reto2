c7ae464eb3ddc72c669ce8759c8f6275cd0d5edd
Jon Maneiro <jon.maneiro@ikasle.egibide.org>
2018-11-23 10:52:03 +0100

    AHHHHHHHH

--- Web/php/generar-respuestas.php
+++ Web/php/generar-respuestas.php
@@ -5,7 +5,7 @@
  * Date: 16/11/2018
  * Time: 10:10
  */
-if(session_id()==''){
+if (session_id() == '') {
     session_start();
 }
 /**
@@ -14,12 +14,13 @@ if(session_id()==''){
  *
  * guarda los datos en un array que contiene likes y dislikes
  */
-function numlikesdislikes($valor){
+function numlikesdislikes($valor)
+{
     $tempcontador = array(
-        "likes"=>0,
-        "dislikes"=>0
+        "likes" => 0,
+        "dislikes" => 0
     );
-    if ($valor!=null) {
+    if ($valor != null) {
         foreach ($valor as $item => $value) {
             if ($value == 1) {
                 $tempcontador['likes']++;
@@ -36,28 +37,31 @@ function numlikesdislikes($valor){
  *
  * Genera una respuesta con los datos suministrados
  */
-function generarRespuestasPregunta($datosRespuesta){
-    foreach ($datosRespuesta as $clave=>$valor) {
+function generarRespuestasPregunta($datosRespuesta)
+{
+    foreach ($datosRespuesta as $clave => $valor) {
 
-         $votos = numlikesdislikes($valor);
-         $archivo = explode("_",$valor['archivos']);
+        $votos = numlikesdislikes($valor);
+        $archivo = explode("_", $valor['archivos']);
         ?>
         <article class="contenedor-respuesta" id="<?= $valor['idRespuesta'] ?>">
-            <span>por <a href="../pages/perfil.php?usuario=<?= $valor['Usuario_idUsuario'] ?>" class="informacion-usuario-pregunta"><?= $valor['nombre'] ?></a> a <?=$valor['fecha']?></span>
-            <h3 class="titulo-respuesta-pregunta"><?=$valor['titulo']?></h3>
-            <p class="respuesta-pregunta"><?=$valor['cuerpo']?></p>
+            <span>por <a href="../pages/perfil.php?usuario=<?= $valor['Usuario_idUsuario'] ?>"
+                         class="informacion-usuario-pregunta"><?= $valor['nombre'] ?></a> a <?= $valor['fecha'] ?></span>
+            <h3 class="titulo-respuesta-pregunta"><?= $valor['titulo'] ?></h3>
+            <p class="respuesta-pregunta"><?= $valor['cuerpo'] ?></p>
             <?php
-            if($archivo[0] !== ""){
-            ?>
-            <span><a class="informacion-usuario-pregunta" href="<?= $valor['archivos'] ?>"><?= $archivo[1] ?></a></span>
-            <?php
+            if ($archivo[0] !== "") {
+                ?>
+                <span><a class="informacion-usuario-pregunta"
+                         href="<?= $valor['archivos'] ?>"><?= $archivo[1] ?></a></span>
+                <?php
             }
             ?>
             <div class="contenedor-likes-respuesta">
                 <img src="../media/like.png" alt="imagen-like" class="imagen-like">
-                <span class="numero-likes-respuesta"><?=$votos['likes']?></span>
+                <span class="numero-likes-respuesta"><?= $votos['likes'] ?></span>
                 <img src="../media/like.png" alt="imagen-like" class="imagen-dislike">
-                <span class="numero-dislikes-respuesta"><?=$votos['dislikes']?></span>
+                <span class="numero-dislikes-respuesta"><?= $votos['dislikes'] ?></span>
             </div>
         </article>
         <?php
