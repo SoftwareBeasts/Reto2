e35eb6811ede14f052f8e46b68a9b5be38aee99d
Jon Maneiro <jon.maneiro@ikasle.egibide.org>
2018-11-23 10:43:35 +0100

    Documentacion voto_preguntaDB

--- Web/js/index.js
+++ Web/js/index.js
@@ -1,11 +1,11 @@
 /*AHAHHAA*/
 $(function () {
-    if ($("#modoBusquedaPorTexto").length>0){
+    if ($("#modoBusquedaPorTexto").length > 0) {
         $("#contenedor-preguntas-index").load("php/generar-preguntas.php?modoBusqueda=perso");
     }
     else if ($("#boton-seleccion-seleccionado").length == 0) {
         $("[name='recientes']").attr("id", "boton-seleccion-seleccionado");
-        $("#contenedor-preguntas-index").load("php/generar-preguntas.php?modoBusqueda="+$("#boton-seleccion-seleccionado").attr("name"));
+        $("#contenedor-preguntas-index").load("php/generar-preguntas.php?modoBusqueda=" + $("#boton-seleccion-seleccionado").attr("name"));
     }
 
 });
@@ -13,19 +13,19 @@ $(function () {
 $(".boton-selector").click(cambiarBusqueda);
 $(".boton-selector").click(function () {
     $(".boton-selector").removeAttr("disabled");
-    $(this).attr("disabled","disabled");
+    $(this).attr("disabled", "disabled");
 });
 
 /*FUNCIONES*/
 function cambiarBusqueda() {
     $("#boton-seleccion-seleccionado").removeAttr('id');
     $(this).attr("id", "boton-seleccion-seleccionado");
-    $("#contenedor-preguntas-index").load("php/generar-preguntas.php?modoBusqueda="+$("#boton-seleccion-seleccionado").attr("name"));
+    $("#contenedor-preguntas-index").load("php/generar-preguntas.php?modoBusqueda=" + $("#boton-seleccion-seleccionado").attr("name"));
 }
 
 
-function cargarMasPreguntas(){
-    $("#contenedor-preguntas-index").load("php/generar-preguntas.php?modoBusqueda="+$("#botonCargarMasPreguntas").val()
-        +"&cargarMas="+$("#botonCargarMasPreguntas").attr("name"));
+function cargarMasPreguntas() {
+    $("#contenedor-preguntas-index").load("php/generar-preguntas.php?modoBusqueda=" + $("#botonCargarMasPreguntas").val()
+        + "&cargarMas=" + $("#botonCargarMasPreguntas").attr("name"));
 }
 
